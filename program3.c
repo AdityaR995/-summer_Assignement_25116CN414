f55adf09389ff74e6132bd3fed333f8a28d9d0c1
AdityaR995 <adityarawat26102007@gmail.com>
2026-05-23 23:34:26 +0530

    Removed extra files

--- program3.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>;
-int main(){
-    float r,circ;
-    scanf("%f",&r);
-    circ=2*3.14*r;
-    printf("circumference of the circle is%f",circ);
-    return 0;
-}
--- program4.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main(){
-    float c,conv;
-    scanf("%f",&c);
-    conv=(c*1.8)+32;
-    printf("farenheit value is %f",conv);
-    return 0;
-}
